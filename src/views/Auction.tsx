69b7daef0a46597450429ae89a3737eb7fdf806a
Ttebify <ttebify@gmail.com>
2022-02-10 00:36:59 +0100

    update seo description for auction page

--- src/views/Auction.tsx
+++ src/views/Auction.tsx
@@ -12,8 +12,8 @@ export default function Auction() {
       <SEO
         slug="/auction"
         title="Stake to earn more"
-        pageDescription="Stake your ASP tokens within the Staking Portal and earn daily interest.
-            Additionally, Stakers are rewarded AVAX tokens from the daily Lobbies based off the
+        pageDescription="Stake your AVAX tokens within the Auction Portal and earn daily interest.
+            Additionally, Stakers are rewarded ASP tokens from the daily Lobbies based off the
             percentage of total tokens beign staked."
       />
       <div className="px-4 pt-20 max-w-screen-2xl mx-auto">
